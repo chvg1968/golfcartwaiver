fbb227d6b46e4e28beb4f90baf907de3758e8373
Tu Nombre <tu_email@ejemplo.com>
2025-02-25 16:07:33 -0500

    Update node modules and config

--- vite.config.js
+++ vite.config.js
@@ -28,13 +28,10 @@ export default defineConfig({
             }
         }
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '@services': path.resolve(__dirname, './src/services'),
-            '@utils': path.resolve(__dirname, './src/utils'),
-            '@components': path.resolve(__dirname, './src/components'),
-            '@config': path.resolve(__dirname, './src/config')
-        }
-    }
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false,
+        minify: true
+      }
 });
